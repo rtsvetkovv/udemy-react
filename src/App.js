fc52d7c516e2019497fb08094c7f539a57617c80
Roman Tsvetkov <tsvetkovv22@gmail.com>
2018-10-16 00:53:48 +1000

    Styling React Components & Elements finished

--- src/App.js
+++ src/App.js
@@ -43,16 +43,8 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    };
-
     let persons = null;
+    let btnClass = "";
 
     if (this.state.showPersones) {
       persons = (
@@ -70,7 +62,7 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = "red";
+      btnClass = classes.Red;
     }
 
     const assignClasses = [];
@@ -85,7 +77,7 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignClasses.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
