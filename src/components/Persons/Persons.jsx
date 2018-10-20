fff45266530f02d891919c106d2c49c51ecf61e8
Roman Tsvetkov <tsvetkovv22@gmail.com>
2018-10-20 22:34:02 +1000

    Lifecycle methods started

--- src/components/Persons/Persons.jsx
+++ src/components/Persons/Persons.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Person from "./Person/Person";
 
-const Persons = ({ persons, clicked, changed }) => {
-  return persons.map((person, index) => {
-    return (
-      <Person
-        click={() => clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={index}
-        changed={event => changed(event, person.id)}
-      />
-    );
-  });
-};
+class Persons extends Component {
+  render() {
+    const { persons, clicked, changed } = this.props;
+    return persons.map((person, index) => {
+      return (
+        <Person
+          click={() => clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={index}
+          changed={event => changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
 
 export default Persons;
